9338c19335e0ecc0deb9f4dcfcd809174becf64b
nabeel <nabeel@example.com>
2019-11-28 00:35:10 +0530

    starting page with logo and cross button (frameless)

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const electron = require('electron')
 
-const { app, BrowserWindow } = electron
+const { app, BrowserWindow, Menu } = electron
+
+Menu.setApplicationMenu(null)
 
 app.on('ready', () => {
-    var win = new BrowserWindow({ width: 600, height: 512, frame: false })
-    win.loadURL("https://www.google.com")
+    var win = new BrowserWindow({ width: 1200, height: 700, frame: false })
+    win.removeMenu(null)
+    win.loadFile("index.html")
+        // win.loadURL("https://app.clubhouse.io/")
 })

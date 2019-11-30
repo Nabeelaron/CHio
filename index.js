e06436d6e454e04be6187c116877c05872a7dba1
nabeel <nabeel@example.com>
2019-11-30 17:02:30 +0530

    Implemented StartPage animation

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const electron = require('electron')
 
 const { app, BrowserWindow, Menu } = electron
 
-Menu.setApplicationMenu(null)
+// Menu.setApplicationMenu(null)
 
 app.on('ready', () => {
-    var win = new BrowserWindow({ width: 1200, height: 700, frame: false })
-    win.removeMenu(null)
+    var win = new BrowserWindow({ width: 1200, height: 700, frame: true })
     win.loadFile("index.html")
         // win.loadURL("https://app.clubhouse.io/")
 })

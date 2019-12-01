49248d394d5461c749f65921d3136592c6289a80
nabeel <nabeel@example.com>
2019-12-01 15:33:45 +0530

    Merge branch 'internet_check'

---
+++ check.js
@@ -0,0 +1,26 @@
+function check() {
+    // var XMLHttpRequest = XMLHttpRequest;
+    var xhr = new XMLHttpRequest();
+    //index.php is in my web
+    xhr.open('HEAD', 'https://www.google.com', true);
+    xhr.send();
+    xhr.addEventListener("readystatechange", processRequest, true);
+
+
+    function processRequest(e) {
+        // var result;
+        if (xhr.readyState == 4) {
+            //If you use a cache storage manager (service worker), it is likely that the
+            //index.php file will be available even without internet, so do the following validation
+            if (xhr.status >= 200 && xhr.status < 304) {
+                //online
+                console.log("online");
+            } else {
+
+                window.location.href = "resources\\nointernet.html";
+            }
+        }
+    }
+}
+
+// check()

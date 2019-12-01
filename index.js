bcaed52266e492d7a935026de269c5396e5145d6
nabeel <nabeel@example.com>
2019-12-01 13:19:14 +0530

    no internet connection feature implemented

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const electron = require('electron')
 
-const { app, BrowserWindow, Menu } = electron
+const { app, BrowserWindow, Menu, ipcMain } = electron
 
 // Menu.setApplicationMenu(null)
 
 app.on('ready', () => {
-    var win = new BrowserWindow({ width: 1200, height: 700, frame: true })
+    var win = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 1200,
+        height: 700,
+        frame: true
+    })
     win.loadFile("index.html")
         // win.loadURL("https://app.clubhouse.io/")
+})
+
+ipcMain.on('check', (event, text) => {
+
 })
